7afedf157a47f48ee737bbccb5531fa74bc459c2
Valdemar <gravemaggot1@gmail.com>
2020-05-17 14:13:03 +0300

    dvaaaa

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+#test git
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
