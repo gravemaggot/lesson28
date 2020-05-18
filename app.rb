044d9e55c1119b37902f0d37f389b96df74cbb03
Valdemar <gravemaggot1@gmail.com>
2020-05-18 21:29:36 +0300

    Save content to sqlite

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
 
 #test git
+
+def init_db
+	@db = SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end
+
+configure do
+	init_db
+	
+	@db.execute 'CREATE TABLE IF NOT EXISTS Posts
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_date DATE,
+		content TEXT
+	)'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -11,4 +32,17 @@ end
 
 get '/new' do
 	erb :new
+end
+
+post '/new' do
+	content = params[:content]
+
+	if content.length <= 0 
+		@error = 'Type post text'
+		return erb :new
+	end
+
+	@db.execute 'INSERT INTO Posts (content, created_date) values (?, datetime())', [content]
+
+	erb "You tiped #{content}"
 end
